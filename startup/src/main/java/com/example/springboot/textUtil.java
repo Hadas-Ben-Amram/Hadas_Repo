d4158223c989dca7dfbf3d43ead04d3dfe3b07eb
הדס בן עמרם <212365803@mbydomain>
2021-11-02 20:01:55 +0200

    init

---
+++ startup/src/main/java/com/example/springboot/textUtil.java
@@ -0,0 +1,16 @@
+package com.example.springboot;
+
+import java.util.*;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+
+public class TextUtils{
+    public string getText(){
+        return "my func return the string";
+    }
+}
